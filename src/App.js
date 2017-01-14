54d8563b7cdaf39fddb5647ba0f04a63c351e4bc
David Latimore II <dgl@u.northwestern.edu>
2017-01-13 20:58:13 -0600

    Initialized the React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Welcome to A&O </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
