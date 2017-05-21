b4da8989e02155df4897dfb98b6e4bad15a793c5
yawkity <sehmon3@gmail.com>
2017-05-20 23:42:19 -0500

    Added a bunch of shit lmaooooo

--- imports/ui/pages/poll/poll.js
+++ imports/ui/pages/poll/poll.js
@@ -1,6 +1,7 @@
 import './poll.html';
 
-import '../../components/poll/item/item.js';
+import '../../components/poll/photoItem/photoItem.js';
+import '../../components/poll/textItem/textItem.js';
 
 Template.poll.helpers({
     artistList: function() {
@@ -53,6 +54,68 @@ Template.poll.helpers({
         }
         return row_list;
     },
+    genderQuestions: function() {
+        genderList = [
+            {text: "Genderqueer or gender questioning"},
+            {text: "Intersex, disorders of sex development, two-spirit, or other related terms"},
+            {text: "Man"},
+            {text: "Nonconforming"},
+            {text: "Transgender"},
+            {text: "Woman"},
+            {text: "I prefer not to respond"}
+        ];
+        return genderList;
+    },
+    raceQuestions: function() {
+        raceQuestions = [
+            {text: "American Indian or Alaskan Native"},
+            {text: "Asian or Asian-American"},
+            {text: "Black or African-American"},
+            {text: "Hispanic or Latinx"},
+            {text: "Middle Eastern"},
+            {text: "Native Hawaiian or Pacific Islander"},
+            {text: "Two or more races"},
+            {text: "White"},
+            {text: "I prefer not to respond"}
+        ];
+        return raceQuestions;
+    },
+    genreQuestions: function() {
+        genreQuestions = [
+            {text: "Blues"},
+            {text: "Country"},
+            {text: "Electronic"},
+            {text: "Folk"},
+            {text: "Hip-Hop"},
+            {text: "Jazz"},
+            {text: "Reggae"},
+            {text: "Alternative"},
+            {text: "R&B/Soul"},
+            {text: "Pop"},
+            {text: "Dance"},
+            {text: "Rock"},
+            {text: "Rap"}
+        ];
+        return genreQuestions;
+    },
+    favoriteEventQuestions: function() {
+        favoriteEventQuestions = [
+            {text: "Ball (Jeremih & Aminé at Riviera Theatre)"},
+            {text: "Blowout (Young Thug & Kehlani at Welsh-Ryan Arena)"},
+            {text: "Fall Lineup (Finding Dory, The Nice Guys, The Jungle Book, Southside with You)"},
+            {text: "Winter Q&A Speaker (Samantha Bee & Rebecca Traister)"},
+            {text: "Winter Lineup (Arrival, Moana, Loving, Fantastic Beasts and Where to Find Them)"},
+            {text: "B-Fest"},
+            {text: "Chicago Benefit (Noname, Jamila Woods, Rachel Williams, Rebecca O’Neil)"},
+            {text: "Spring Films Speaker (Moonlight with Barry Jenkins)"},
+            {text: "Philfest (Margaret Glaspy & Sanctified Grumblers)"},
+            {text: "Comedy Fest (Chris Gethard & Aparna Nancherla)"},
+            {text: "Spring Lineup (Space Jam, Rogue One, Lego Batman, Get Out)"},
+            {text: "Spring Standup Comedy Speaker (Michael Che & Julio Torres)"},
+            {text: "I did not attend any"}
+        ];
+        return favoriteEventQuestions;
+    }
 });
 
 Template.poll.onCreated(function() {
@@ -103,23 +166,27 @@ Template.poll.events({
 
         console.log(artistSelectedPairs);
 
-        // Validation:
-
-        // 1. Validate against original list of artists
-        // Meteor.call('insertResponse', artistSelectedPairs, (error) => {
-        //     if (error) {
-        //         console.log(error);
-        //     }
-        // });
-
     },
     'click .poll-item' (event) {
         var name = event.target.name;
-        if ($('img[data-name="' + name + '"]').hasClass('grey-image')){
-            $('img[data-name="' + name + '"]').removeClass('grey-image');
+        if ($('img[data-name="' + name + '"]').hasClass('selected-item')){
+            $('img[data-name="' + name + '"]').addClass('deselected-item');
+            $('img[data-name="' + name + '"]').removeClass('selected-item');
         } else {
-            $('img[data-name="' + name + '"]').addClass('grey-image');
+            $('img[data-name="' + name + '"]').addClass('selected-item');
+            $('img[data-name="' + name + '"]').removeClass('deselected-item');
+        }
+    },
+    'click .text-item' (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log(event.currentTarget.getElementsByClassName('poll-item')[0].checked);
+        event.currentTarget.getElementsByClassName('poll-item')[0].checked = !event.currentTarget.getElementsByClassName('poll-item')[0].checked;
+
+        if(event.currentTarget.getElementsByClassName('poll-item')[0].checked){
+            event.currentTarget.classList.add('selected-item');
+        } else {
+            event.currentTarget.classList.remove('selected-item');
         }
     }
-
 });
