c1178c410f4e55fa9f18a159c5ad50bab89ea67b
yawkity <sehmon3@gmail.com>
2017-05-23 16:36:07 -0500

    Removed starter app code

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,5 @@
 // Import server startup through a single index entry point
 
-import './fixtures.js';
 import './register-api.js';
 
 Meteor.startup(function() {
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,7 +1,4 @@
 // Register your apis here
 
-import '../../api/links/methods.js';
-import '../../api/links/server/publications.js';
-
 import '../../api/PollResponses/PollResponses.js'
 import '../../api/PollResponses/methods.js'
