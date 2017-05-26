1bd2e10fea8d53aa73bc29268273308b208c4db4
yawkity <sehmon3@gmail.com>
2017-05-26 12:22:17 -0500

    Added comments discusing methods for server-side poll validation

--- server/main.js
+++ server/main.js
@@ -3,12 +3,20 @@
 import '/imports/startup/server';
 import '/imports/startup/both';
 
+import { check } from 'meteor/check'
+
 Meteor.methods({
   insertResponse: function(response) {
       // To prevent spam responses
       // If the user fills out the poll quicker than 30 seconds, assume they're a bot and ignore the response
       new_response = response;
       new_response.endTimestamp = new Date();
+
+      // Validation Process
+      // 1. Check time taken to complete poll and compare to threshold
+      // 2. Validate each question to make sure the length of responses to each question is valid
+      // 3. Validate each question to make sure the name is valid and value is a boolean
+
 
       var threshold = 30;
       timeDiff = (new_response.endTimestamp.getTime() - new_response.startTimestamp.getTime()) / 1000;
